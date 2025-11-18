0efe3cf3283a34db7ffa59b666c7ab9a0976160d
psissinto <psissinto@gmail.com>
2025-11-18 18:57:49 -0500

    Adding main.py for railway.com deployment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template_string
 from pay import PayClass
 import time
+import os
 
 app = Flask(__name__)
 
@@ -344,5 +345,8 @@ def failed_screen():
 # RUN SERVER
 # -----------------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
